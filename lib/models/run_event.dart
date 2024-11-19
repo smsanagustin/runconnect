365d159d88797bebea5b4c39f3dfbcba275bd8b8
Sophie San Agustin <smsanagustin@up.edu.ph>
2024-11-20 04:50:30 +0800

    feat: Add firestore serializer for the runevent class

--- lib/models/run_event.dart
+++ lib/models/run_event.dart
@@ -1,21 +1,10 @@
-import 'package:runconnect/models/comment.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class RunEvent {
-  String? id;
-  String? name;
-  String? location;
-  String? meetupPlace;
-  String? time;
-  int? distance;
-  int? numberOfParticipants;
-  String? date;
-  String? runType;
-  List<Comment> comments;
-
   // constructor
   RunEvent({
     required this.id,
-    required this.name,
+    required this.title,
     required this.location,
     required this.meetupPlace,
     required this.time,
@@ -23,6 +12,67 @@ class RunEvent {
     required this.numberOfParticipants,
     required this.date,
     required this.runType,
-    this.comments = const [],
-  });
+    List<String>? comments,
+  }) : _comments = comments ?? [];
+
+  String? id;
+  String? title;
+  String? location;
+  String? meetupPlace;
+  String? time;
+  int? distance;
+  int? numberOfParticipants;
+  String? date;
+  String? runType;
+  List<String> _comments;
+
+  // getter for a comment
+  List<String> get comments => List.unmodifiable(_comments);
+
+  // add a comment to this event
+  void addComment(String commentId) {
+    _comments.add(commentId);
+  }
+
+  // method to send object's data to firestore
+  Map<String, dynamic> toFirestore() {
+    return {
+      'id': id,
+      'title': title,
+      'location': location,
+      'meetupPlace': meetupPlace,
+      'time': time,
+      'distance': distance,
+      'numberOfParticipants': numberOfParticipants,
+      'date': date,
+      'runType': runType,
+      'comments': comments,
+    };
+  }
+
+  // method to convert a received data from firestore to a RunEvent object
+  factory RunEvent.fromFirestore(
+      DocumentSnapshot<Map<String, dynamic>> snapshot,
+      SnapshotOptions? options) {
+    final data = snapshot.data()!;
+
+    // make a RunEvent instance
+    RunEvent runEvent = RunEvent(
+        id: data['id'],
+        title: data['title'],
+        location: data['location'],
+        meetupPlace: data['meetupPlace'],
+        time: data['time'],
+        distance: data['distance'],
+        numberOfParticipants: data['numberOfParticipants'],
+        date: data['date'],
+        runType: data['runType']);
+
+    // get list of comments
+    for (String commentId in data['comments']) {
+      runEvent.addComment(commentId);
+    }
+
+    return runEvent;
+  }
 }
--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -3,8 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:runconnect/models/app_user.dart';
 
 class FirestoreService {
-  // add a reference to the characters collection
-  static final ref = FirebaseFirestore.instance
+  // add a reference to the users collection
+  static final userRef = FirebaseFirestore.instance
       .collection("appUsers")
       .withConverter(
           fromFirestore: AppUser.fromFirestore,
@@ -12,18 +12,18 @@ class FirestoreService {
 
   // add user to firestore
   static Future<void> addUser(AppUser appUser) async {
-    await ref.doc(appUser.uid).set(appUser);
+    await userRef.doc(appUser.uid).set(appUser);
   }
 
   // get a user after they've signed in or signed up
   static Future<AppUser?> getUser(String uid) async {
-    final doc = await ref.doc(uid).get();
+    final doc = await userRef.doc(uid).get();
     return doc.data();
   }
 
   // update user's profile in firestore
   static Future<void> updateUser(AppUser appUser) async {
-    await ref.doc(appUser.uid).update({
+    await userRef.doc(appUser.uid).update({
       'fullName': appUser.fullName,
       'username': appUser.username,
       'location': appUser.location
