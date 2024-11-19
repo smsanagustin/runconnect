f39858bb48df003fcacbf80f689bcc5362048190
Sophie San Agustin <smsanagustin@up.edu.ph>
2024-11-19 10:07:48 +0800

    ui: Add a time picker

--- lib/screens/home/host.dart
+++ lib/screens/home/host.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:runconnect/shared/styled_text.dart';
 import 'package:runconnect/theme.dart';
 
@@ -10,6 +11,18 @@ class HostScreen extends StatefulWidget {
 }
 
 class _HostScreenState extends State<HostScreen> {
+  // stores the date and time picked by the user
+  TextEditingController dateInput = TextEditingController();
+  TextEditingController timeInput = TextEditingController();
+
+  @override
+  void initState() {
+    dateInput.text = ""; //set the initial value of text field
+    timeInput.text = "";
+    super.initState();
+  }
+
+  // used to control the form
   final _formGlobalKey = GlobalKey<FormState>();
 
   @override
@@ -38,6 +51,7 @@ class _HostScreenState extends State<HostScreen> {
                   // title
                   TextFormField(
                       decoration: InputDecoration(
+                        icon: const Icon(Icons.title),
                         label: const StyledText("Run title"),
                         border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(10),
@@ -52,9 +66,91 @@ class _HostScreenState extends State<HostScreen> {
                           return "Your event must have a name.";
                         }
                         return null;
-                      })
+                      }),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: dateInput,
+                    decoration: InputDecoration(
+                      icon: const Icon(Icons.calendar_today),
+                      labelText: "Enter date",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: AppColors.textColor)),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              color: AppColors.textColor, width: 2.0)),
+                    ),
+                    readOnly: true,
+                    onTap: () async {
+                      DateTime? pickedDate = await showDatePicker(
+                          context: context,
+                          initialDate:
+                              DateTime.now().add(const Duration(days: 1)),
+                          firstDate:
+                              DateTime.now().add(const Duration(days: 1)),
+                          lastDate: DateTime(2100));
+                      if (pickedDate != null) {
+                        setState(() {
+                          // format the date picked by the user and store it
+                          dateInput.text =
+                              DateFormat('yyyy-MM-dd').format(pickedDate);
+                        });
+                      }
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Your event must have a date.";
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: timeInput,
+                    decoration: InputDecoration(
+                      icon: const Icon(Icons.schedule),
+                      labelText: "Enter time",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: AppColors.textColor)),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              color: AppColors.textColor, width: 2.0)),
+                    ),
+                    readOnly: true,
+                    onTap: () async {
+                      TimeOfDay? pickedTime = await showTimePicker(
+                        context: context,
+                        initialTime: TimeOfDay.now(),
+                      );
+                      if (pickedTime != null) {
+                        setState(() {
+                          // format the time picked by the user and store it
+                          setState(() {
+                            timeInput.text = pickedTime.format(context);
+                          });
+                        });
+                      }
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Your event must have a time.";
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
                 ],
               ),
             )));
   }
 }
+
+// REFERENCE: https://mobikul.com/date-picker-in-flutter/#:~:text=DatePicker%20is%20a%20material%20widget,by%20calling%20flutter's%20inbuilt%20function.
