88c31c0b2cf68474b2640e1f4a4d926a42acce41
Sophie San Agustin <smsanagustin@up.edu.ph>
2025-01-03 00:23:17 +0800

    ui: Remove padding at the top in feed screen when scrolling

--- lib/screens/home/feed.dart
+++ lib/screens/home/feed.dart
@@ -67,69 +67,76 @@ class _FeedScreenState extends ConsumerState<FeedScreen>
               : const CircularProgressIndicator(),
         ),
         body: Padding(
-            padding: const EdgeInsets.all(16),
+            padding: const EdgeInsets.symmetric(horizontal: 18),
             child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: AppColors.primaryColor,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 14, horizontal: 18),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            const Icon(Icons.directions_run),
-                            appUser.isNotEmpty
-                                ? StyledTitleMedium(
-                                    "Runs near ${appUser.first.location.split(',').first}") // show only the locality
-                                : const StyledTitleMedium("Runs near you"),
-                          ],
-                        ),
-                        IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Icons.add_location_alt))
-                      ],
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  StreamBuilder(
-                    stream: EventFirestoreService.getEvents(),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const Center(child: CircularProgressIndicator());
-                      } else if (snapshot.hasError) {
-                        return Center(child: Text('Error: ${snapshot.error}'));
-                      } else if (!snapshot.hasData ||
-                          snapshot.data!.docs.isEmpty) {
-                        return const Center(child: Text('No events available'));
-                      }
-
-                      return ListView.builder(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemCount: snapshot.data!.docs.length,
-                        itemBuilder: (context, index) {
-                          RunEvent event = snapshot.data!.docs[index].data();
-                          return Column(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 18),
+                child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: AppColors.primaryColor,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 14, horizontal: 18),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
                             children: [
-                              RunEventContainer(runEvent: event),
-                              const SizedBox(
-                                height: 10,
-                              )
+                              const Icon(Icons.directions_run),
+                              appUser.isNotEmpty
+                                  ? StyledTitleMedium(
+                                      "Runs near ${appUser.first.location.split(',').first}") // show only the locality
+                                  : const StyledTitleMedium("Runs near you"),
                             ],
-                          );
-                        },
-                      );
-                    },
-                  ),
-                ],
+                          ),
+                          IconButton(
+                              onPressed: () {},
+                              icon: const Icon(Icons.add_location_alt))
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    StreamBuilder(
+                      stream: EventFirestoreService.getEvents(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        } else if (snapshot.hasError) {
+                          return Center(
+                              child: Text('Error: ${snapshot.error}'));
+                        } else if (!snapshot.hasData ||
+                            snapshot.data!.docs.isEmpty) {
+                          return const Center(
+                              child: Text('No events available'));
+                        }
+
+                        return ListView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (context, index) {
+                            RunEvent event = snapshot.data!.docs[index].data();
+                            return Column(
+                              children: [
+                                RunEventContainer(runEvent: event),
+                                const SizedBox(
+                                  height: 10,
+                                )
+                              ],
+                            );
+                          },
+                        );
+                      },
+                    ),
+                  ],
+                ),
               ),
             )));
   }
